32894ab1e9b50e4cb0547f7d829f65b891435ccc
Donstesh <stephenshifoko@gmail.com>
2023-01-18 09:49:45 +0300

    Updated Add Chef and add Waiter

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,41 +2,40 @@ import Head from 'next/head'
 import Styles from "../styles/Home.module.css"
 import Header from './Comp/Header'
 import Main from './Comp/Main'
-import UpdateToppins from './Comp/Update.Toppin'
-import UpdateChef from './Comp/doughchef'
+import UpdateToppins from './Comp/Updatetoppin'
+import UpdateChef from './Comp/Doughchef'
+import UpdateWaiter from './Comp/Waiter'
 import Base from './Comp/Base'
 import { useState } from 'react'
 import CartBox from './Comp/CartBox'
 import { AnimatePresence } from 'framer-motion'
-import CheakOrder from './Comp/CheakOrder'
+import CheckOrder from './Comp/CheckOrder'
 export default function Index() {
   const [renderIndex, setRenderIndex] = useState(0)
   const [selectedChef, setSelectedChef] = useState([])
   const [selectedToppins, setSelectedToppins] = useState([])
   const [selectedBase, setSelectedBase] = useState("default")
+  const [selectedWaiter, setSelectedWaiter] = useState([])
   const [toggleCart, setToggleCart] = useState(false)
   return (
     <>
       <Head>
         {/* Simple SEO  */}
-        <title>Pizza Website UI</title>
-        <meta name="description" content="Pizza website UI Project create with the help of ReactJS ,NextJS ,Framer-Motion. " />
+        <title>PizzaS</title>
+        <meta name="description" content="PizzaS Project created with the help of ReactJS ,NextJS ,Framer-Motion. " />
         <meta name="robots" content="noindex, nofollow" /> </Head>
       <div className={Styles.wrapper}>
         <div className={Styles.inner}>
           <Header setToggleCart={setToggleCart} toggleCart={toggleCart} />
           {renderIndex == 0 ? <Main toNext={setRenderIndex} /> : null}
-          {renderIndex == 1 ? <UpdateChef toNext={setRenderIndex} updateToppins={setSelectedChef} /> : null}
+          {renderIndex == 1 ? <UpdateChef toNext={setRenderIndex} UpdateChef={setSelectedChef} /> : null}
           {renderIndex == 2 ? <UpdateToppins toNext={setRenderIndex} updateToppins={setSelectedToppins} /> : null}
-          {renderIndex == 3 ? <Base toNext={setRenderIndex}
-            updateBase={setSelectedBase} /> : null}
-          {renderIndex == 4 ? <CheakOrder toNext={setRenderIndex}
-            updateBase={setSelectedBase} selectedToppins={selectedToppins}
-            selectedBase={selectedBase} /> : null}
+          {renderIndex == 3 ? <Base toNext={setRenderIndex} updateBase={setSelectedBase} /> : null}
+          {renderIndex == 4 ? <UpdateWaiter toNext={setRenderIndex} UpdateWaiter={setSelectedWaiter} /> : null}
+          {renderIndex == 5 ? <CheckOrder toNext={setRenderIndex} updateBase={setSelectedBase} selectedChef={selectedChef} selectedToppins={selectedToppins} selectedBase={selectedBase} selectedWaiter={selectedWaiter} /> : null}
         </div>
         <AnimatePresence>
-          {toggleCart && <CartBox selectedToppins={selectedToppins}
-            selectedBase={selectedBase} />}
+          {toggleCart && <CartBox selectedChef={selectedChef} selectedToppins={selectedToppins} selectedBase={selectedBase} selectedWaiter={selectedWaiter}  />}
         </AnimatePresence>
       </div >
     </>
