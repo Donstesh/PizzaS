aba9f09ffff7920c1c91a34514a398885a18ba5e
Donstesh <stephenshifoko@gmail.com>
2023-01-18 09:50:55 +0300

    Added Waiter and Chef

--- pages/Comp/CartBox.jsx
+++ pages/Comp/CartBox.jsx
@@ -1,18 +1,17 @@
-// -C 
-// Required Stuff
 import classes from "../../styles/CartBox.module.css"
 import { motion } from 'framer-motion'
-export default function CartBox({ selectedToppins, selectedBase, position, L, R, B, T }) {
+export default function CartBox({ selectedChef, selectedToppins, selectedBase, selectedWaiter,  position, L, R, B, T }) {
     // Remove Duplicate 
     const filteredToppins = [...new Set(selectedToppins)]
-    // mapping toppins list 
+
     const tpList = filteredToppins.map(v => {
         return (<li key={v}>{v}</li>)
     })
+
     return (<motion.div style={{ height: 0 }}
         animate={
             {
-                height: 300,
+                height: 350,
                 position: position,
                 left: L,
                 right: R,
@@ -22,7 +21,11 @@ export default function CartBox({ selectedToppins, selectedBase, position, L, R,
         }
         exit={{ height: 0 }} transition={{ duration: .3 }}
         className={classes.wrapper}>
-        <h3>Shopping Cart</h3>
+        <h3>Order Cart</h3> <h4>Waiting Time 5 Seconds</h4>
+        <div>
+            <h4>Chef - </h4>
+            <p> {selectedChef} </p>
+        </div>
         <div>
             <h4>Toppins -</h4>
             <ul> {tpList} </ul>
@@ -30,6 +33,10 @@ export default function CartBox({ selectedToppins, selectedBase, position, L, R,
         <div>
             <h4>Base - </h4>
             <p>{selectedBase}</p>
+        </div>
+        <div>
+            <h4>Waiter - </h4>
+            <p>{selectedWaiter}</p>
         </div>
     </motion.div>
 
